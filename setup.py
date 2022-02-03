e9806328b18c4511b7388feac389e0792c9d2c16
mabdelhack <m.abouelyazid@gmail.com>
2022-02-03 09:48:13 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 from distutils.core import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+  long_description = fh.read()
+
 setup(
   name = 'et2adem',         # How you named your package folder (MyLib)
+  long_description = long_description,
+  long_description_content_type = "text/markdown",
   packages = ['et2adem'],   # Chose the same as "name"
   package_dir={'et2adem': 'et2adem'},
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.5',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A more fun version of tqdm',   # Give a short description about your library
   author = 'Mohamed Abdelhack',                   # Type in your name
   author_email = 'mohamed.abdelhack.37a@kyoto-u.jp',      # Type in your E-Mail
-  url = 'https://mabdelhack.github.io/',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/mabdelhack/et2adem/archive/refs/tags/v_01.tar.gz',    # I explain this later on
+  url = 'https://github.com/mabdelhack/et2adem',   # Provide either the link to the package's github or website
+  download_url = 'https://github.com/mabdelhack/et2adem/archive/refs/tags/v_02.tar.gz',    # I explain this later on
   keywords = ['tqdm', 'fun'],   # Keywords that define your package best
   package_data={'et2adem': ['data/*.wav']},
-  data_files = [('', ['et2adem/data/et2adem.wav'])],
   install_requires=[            # I get to this in a second
           'tqdm',
           'playsound==1.2.2',
