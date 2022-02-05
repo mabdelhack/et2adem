f64314ee613450b72d0e4f9e2133d8248195e35f
mabdelhack <m.abouelyazid@gmail.com>
2022-02-05 02:47:38 -0500

    add custom song possibility

--- et2adem/et2adem.py
+++ et2adem/et2adem.py
@@ -5,8 +5,9 @@ import os
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 class et2adem(tqdm):
-    def __init__(self, loop_data, sound=True):
-        self.sound = sound
+    def __init__(self, loop_data, boring=False, mysong=None):
+        self.sound = ~boring
+        self.custom_song = mysong
         tqdm.__init__(self, loop_data,
                       bar_format='{l_bar}{bar}{r_bar}',
                       ascii='\U0001f4a8\U0001f3c3',
@@ -20,9 +21,11 @@ class et2adem(tqdm):
         stop_threads = True
         return
 
-    @staticmethod
-    def play_song(stop):
-        path = 'et2dm.wav'
+    def play_song(self, stop):
+        if self.custom_song is None:
+            path = 'et2dm.wav'
+        else:
+            path = self.custom_song
         while True:
             playsound(os.path.join(_ROOT, 'data', path))
             if stop():
